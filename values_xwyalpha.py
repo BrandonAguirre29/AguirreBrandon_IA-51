c227e846a9cf76e270028e3858fd44db8bff4e90
BrandonAguirre29 <tic-320085@utnay.edu.mx>
2026-02-05 08:20:54 -0700

    lab2 corregido

--- values_xwyalpha.py
+++ values_xwyalpha.py
@@ -1,12 +1,12 @@
 
 #! Estos valores son lo correspondiente a las habilidad del personaje, ataque, defensa, velocidad
 X = [
+    [1, 4],
     [2, 5],
-    [4, 6],
-    [6, 7]
+    [3, 6]
 ]
 
-y = [11, 18, 25]    # daño real o resultado de la batalla
-w = [1.0, 1.0]      # El nivel y/o experiencia
+y = [1, 2, 3]    # daño real o resultado de la batalla
+w = [7, 8]      # El nivel y/o experiencia
 alpha = 0.01        # Que tan rapido aprendera segun su naturaleza
 
