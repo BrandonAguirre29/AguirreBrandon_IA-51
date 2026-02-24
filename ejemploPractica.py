643dc4c5828916bb8e4eb087379c4a6ed36b8717
BrandonAguirre29 <tic-320085@utnay.edu.mx>
2026-02-23 22:42:34 -0700

    a

--- ejemploPractica.py
+++ ejemploPractica.py
@@ -1,9 +1,11 @@
+import matplotlib.pyplot as plt
+
 x = []
 y = []
 
 
 #Leer .csv, lo guarda como lista, cada fila es una cadena
-with open("open.csv", "r") as f:
+with open("practica2_ml.csv", "r") as f:
     lineas = f.readlines()
 
 # separar datos conocidos
@@ -36,12 +38,37 @@ for linea in lineas[1:]:
 
     nuevas_lineas.append(f"{persona},{altura},{peso}\n") #Reconstruye fila
 
-with open("open.csv", "w") as f: #Guardar archivo nuevo, "w" borra archivo viejo y escribe el nuevo
+with open("practica2_ml.csv", "w") as f: #Guardar archivo nuevo, "w" borra archivo viejo y escribe el nuevo
     f.writelines(nuevas_lineas)
+
+
+# ====== GRAFICA ======
+
+# Puntos originales
+plt.scatter(x, y)
+
+# Crear valores para la recta
+x_linea = [min(x), max(x)]
+y_linea = [m * xi + b for xi in x_linea]
+
+# Dibujar recta de regresión
+plt.plot(x_linea, y_linea)
+
+# Etiquetas
+plt.xlabel("Altura (m)")
+plt.ylabel("Peso (kg)")
+plt.title("Prediccion de pesos 1.10, 1.15 y 1.20 metros")
+
 
 
 print("La pendiente (m) es = ",m)
 print("El intercepto (b) es = ",b)
 print("Ecuación de regresión:")
+print("y = mx + b")
 print(f"y = {round(m,4)} * altura + {round(b,4)}")
+print("El peso de la persona que mide 1.10 metros es 20 kg")
+print("El peso de la persona que mide 1.15 metros es 21 kg")
+print("El peso de la persona que mide 1.20 metros es 22 kg")
+
+plt.show()
 
